9124b3b16a5c24dc5f709ffd2429a0894c835e0a
Joshua Harris <joshua40harris@gmail.com>
2015-03-16 22:23:43 -0400

    added machine type to init

--- init_instances.sh
+++ init_instances.sh
@@ -2,6 +2,7 @@
 
 gcloud compute instances create www1 \
 	--zone us-central1-f \
+	--machine-type n1-standard-1 \
 	--image centos-7 \
 	--tags www-tag \
 	--metadata-from-file startup-script=./www_startup.sh
