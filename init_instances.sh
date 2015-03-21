b7642582b9825ce4717dc7188490e83c008e644a
Joshua Harris <joshua40harris@gmail.com>
2015-03-20 20:03:13 -0400

    changed init instance script name

--- init_instances.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-gcloud compute instances create www1 \
-	--zone us-central1-f \
-	--address address-www1 \
-	--machine-type n1-standard-1 \
-	--image centos-7 \
-	--tags www-tag \
-	--metadata-from-file startup-script=www_startup.sh
