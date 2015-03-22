f888b5249915063e495466d1bf2451f3a4bb08f7
Joshua Harris <joshua40harris@gmail.com>
2015-03-21 20:59:57 -0400

    Changed images into folders and links to bitly

--- games.js
+++ games.js
@@ -1,34 +1,34 @@
 module.exports = exports = [
 	{
-		img_path: "/images/TapTitan.jpg",
+		img_path: "/images/games/TapTitan.jpg",
 		id: "tap-titans",
 		title: "Tap Titans",
 		description: "Tap Titans is at the pinnacle of rewarding level up systems.  Each level checkpoint that a hero hits unlocks a special ability which could help you get more gold or boost the power of other heros, all while helping you go from doing one damage to making 100 Trillion damage seems like stubbing your toe.",
-		android_package_name: "com.gamehivecorp.taptitans",
-		ios_path: "tap-titans/id940596201"
+		android_link: "http://bit.ly/bsp-and-tap-titans",
+		ios_link: "http://bit.ly/bsp-ios-tap-titans"
 	},
 	{
-		img_path: "/images/FlappingOnline.jpg",
+		img_path: "/images/games/FlappingOnline.jpg",
 		id: "flapping-online",
 		title: "Flapping Online",
 		description: "Thought you were good at Flappy Bird? Prove it! In Flapping Online you play against other people in 5 minute tournaments to see who's the best at tapping, flapping, and dodging. Oh, and did I mention that there are skin.",
-		android_package_name: "com.havana24.flappingonline",
-		ios_path: "flapping-online/id891340776"
+		android_link: "http://bit.ly/bsp-and-flapping-online",
+		ios_link: "http://bit.ly/bsp-ios-flapping-online"
 	},
 	{
-		img_path: "/images/RunBirdRun.jpg",
+		img_path: "/images/games/RunBirdRun.jpg",
 		id: "run-bird-run",
 		title: "Run Bird Run",
 		description: "Run Bird Run is one poor birds adventure to avoid falling boxes in it's quest for yummy candies. Collecting lots of candy let you unlock new animals (skins) that are hungry for delicious candy... if you can avoid all of those falling boxes.",
-		android_package_name: "com.ketchapp.runbirdrun",
-		ios_path: "run-bird-run/id951346475"
+		android_link: "http://bit.ly/bsp-and-run-bird-run",
+		ios_link: "http://bit.ly/bsp-ios-run-bird-run"
 	},
 	{
-		img_path: "/images/Retry.jpg",
+		img_path: "/images/games/Retry.jpg",
 		id: "retry",
 		title: "RETRY",
 		description: "RETRY is an arcade styled, physics based flying game that makes you weave and barrel roll through stages littered with obstacles. Every tap of a finger is an exhilarating burst of acceleration that could mean certain doom or one more step towards victory.",
-		android_package_name: "com.rovio.retry",
-		ios_path: "retry/id853027301"
+		android_link: "http://bit.ly/bsp-and-retry",
+		ios_link: "http://bit.ly/bsp-ios-retry"
 	}
 ];
